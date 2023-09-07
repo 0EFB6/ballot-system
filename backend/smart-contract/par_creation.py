a55d7116bfecdde5536ff332d11c5ad71987d21f
Wilson Chang <changws12345@gmail.com>
2023-09-07 15:35:12 +0800

    modified:   backend/artifacts/approval.teal 	modified:   backend/artifacts/contract.json 	new file:   backend/smart-contract/par_creation.py 	modified:   backend/smart-contract/test.py

--- backend/smart-contract/par_creation.py
+++ backend/smart-contract/par_creation.py
@@ -6,35 +6,24 @@ AREA_I= Int(5)
 AREA_LEN = Int(25)
 STATE_I = Int(31)
 STATE_LEN = Int(15)
-k_i = Bytes("index")
-
-# box = ""
-#BOX = "011Subang Jaya Selangor"
-# BOX2 = "Selangor Melaka"
-# BOX_SELANGOR = "Subang P104\nPetaling Jaya"
-# BOX_SUBANG_P104 = "Can1 Party1\nCan2 Party2"
-# BOX_MELAKA = "Melaka Tengah\nAlor Gajah"
 
 handle_creation = Seq(
-	App.globalPut(k_i, Int(0)),
 	Approve()
 )
 
 router = Router(
-	"testingg",
+	"Create Parliament Seat",
 	BareCallActions(
 		no_op=OnCompleteAction.create_only(handle_creation),
 	),
 )
 
 @router.method
-def create_par_box(n: abi.String):
-	return Seq(
-		Pop(App.box_create(Concat(Bytes("P"), n.get()), Int(50)))
-	)
+def create_par_box(par_no: abi.String):
+	return Pop(App.box_create(Concat(Bytes("P"), par_no.get()), Int(50)))
 
 @router.method
-def add_par_box(par_n: abi.String, area: abi.String, state: abi.String):
+def create_par_seat(par_n: abi.String, area: abi.String, state: abi.String):
 	return Seq(
 		App.box_replace(Concat(Bytes("P"), par_n.get()), N_I, Concat(Bytes("P"), par_n.get())),
 		App.box_replace(Concat(Bytes("P"), par_n.get()), AREA_I, area.get()),
@@ -53,37 +42,10 @@ def read_par_area(box_no: abi.String, *, output: abi.String):
 def read_par_state(box_no: abi.String, *, output: abi.String):
 	return output.set(App.box_extract(Concat(Bytes("P"), box_no.get()), STATE_I, STATE_LEN))
 
+# Debug Use Only
 @router.method
 def read_debug(n: abi.String, src: abi.Uint64, dst: abi.Uint64,  *, output: abi.String):
 	return output.set(App.box_extract(Concat(Bytes("P"), n.get()), src.get(), dst.get()))
-
-@router.method
-def read_index(*, output: abi.Uint64):
-	return output.set(App.globalGet(k_i))
-
-@router.method
-def increment():
-	scratchCount = ScratchVar(TealType.uint64)
-	return Seq(
-		scratchCount.store(App.globalGet(Bytes("count"))),
-		App.globalPut(Bytes("count"), scratchCount.load() + Int(1)),
-	)
-
-@router.method
-def decrement():
-	scratchCount = ScratchVar(TealType.uint64)
-	return Seq(
-		Assert(Global.group_size() == Int(1)),
-		scratchCount.store(App.globalGet(Bytes("count"))),
-		If(
-			scratchCount.load() > Int(0),
-			App.globalPut(Bytes("count"), scratchCount.load() - Int(1)),
-		),	
-	)
-
-@router.method
-def read_count(*, output:abi.Uint64):
-	return output.set(App.globalGet(Bytes("count")))
 
 if __name__ == "__main__":
 	import os
@@ -93,12 +55,12 @@ if __name__ == "__main__":
 	approval, clear, contract = router.compile_program(version=8)
 
 	# Write out the approval & clear program
-	with open(os.path.join(path, "../artifacts/approval.teal"), "w") as f:
+	with open(os.path.join(path, "../artifacts/par_creation/approval.teal"), "w") as f:
 		f.write(approval)
 
-	with open(os.path.join(path, "../artifacts/clear.teal"), "w") as f:
+	with open(os.path.join(path, "../artifacts/par_creation/clear.teal"), "w") as f:
 		f.write(clear)
 
 	# Dump out the contract as JSON to be used by any SDKs
-	with open(os.path.join(path, "../artifacts/contract.json"), "w") as f:
+	with open(os.path.join(path, "../artifacts/par_creation/contract.json"), "w") as f:
 		f.write(json.dumps(contract.dictify(), indent=2))
