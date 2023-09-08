19e6754c7471e34c72a29776fc6e5e23eef2e8db
Wilson Chang <changws12345@gmail.com>
2023-09-08 16:15:17 +0800

    modified:   contract.py

--- backend/beaker-sc/contract.py
+++ backend/beaker-sc/contract.py
@@ -7,9 +7,24 @@ class InitState:
 		default=Int(69)
 	)
 
-app = Application("Beaker Calculator", state=InitState()).apply(
+class GlobalState:
+	my_desc = GlobalStateValue(
+		stack_type=TealType.bytes,
+		default=Bytes("Hello, World! You funny!"),
+		static=True
+	)
+
+app = Application("Beaker Calculator", state=GlobalState()).apply(
 	unconditional_create_approval, initialize_global_state=True
 )
+
+@app.external
+def set_app_global_state_value(str: abi.String) -> Expr:
+	return app.state.my_desc.set(str.get())
+
+@app.external
+def read_app_global_state_value(*, output: abi.String) -> Expr:
+	return output.set(app.state.my_desc)
 
 @app.external
 def read_state(*, output: abi.Uint64) -> Expr:
