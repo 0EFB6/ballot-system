b8cdf9cabf0fba1c0acdd22f00f21766b6fcc1af
Wilson Chang <changws12345@gmail.com>
2023-09-09 14:37:01 +0800

    Box with beaker still sucks... 	new file:   backend/beaker-sc/archive.py 	modified:   backend/beaker-sc/contract.py 	modified:   backend/beaker-sc/deploy.py

--- backend/beaker-sc/contract.py
+++ backend/beaker-sc/contract.py
@@ -1,135 +1,75 @@
 from beaker import *
 from pyteal import *
+from beaker.lib.storage import BoxList, BoxMapping
 
-# Global State Value
-class InitState:
-	app_state = GlobalStateValue(
-		stack_type=TealType.uint64,
-		default=Int(69)
+class Parliament(abi.NamedTuple):
+	area: abi.Field[abi.String]
+	no: abi.Field[abi.Uint8]
+	state: abi.Field[abi.String]
+	candidate_no: abi.Field[abi.Uint8]
+	candidate_name: abi.Field[abi.String]
+	candidate_party: abi.Field[abi.String]
+	votes: abi.Field[abi.Uint64]
+
+class ParliamentItem:
+	votes = GlobalStateValue(
+		stack_type=TealType.uint64, default=Int(0), descr="Number of votes"
 	)
-'''
-@app.external
-def read_state(*, output: abi.Uint64) -> Expr:
-	return output.set(app.state.app_state)
-'''
+	addr_list = BoxList(abi.Address, 10)
+	par_seat = BoxMapping(abi.String, Parliament)
 
-# Global State Blob
-class BlobState:
-	global_blob = GlobalStateBlob(
-		keys=1
-	)
-'''
-@app.external
-def write_blob(start: abi.Uint64, str: abi.String) -> Expr:
-	return app.state.global_blob.write(start.get(), str.get())
+app = Application("Parliament Item", state=ParliamentItem())
 
 @app.external
-def read_blob(*, output: abi.String) -> Expr:
-	return output.set(
-		app.state.global_blob.read(
-			Int(0), app.state.global_blob.blob.max_bytes - Int(1)
-		)
-	)
-'''
-# Global Static Value (Immutable)
-class GlobalState:
-	my_desc = GlobalStateValue(
-		stack_type=TealType.bytes,
-		default=Bytes("Hello, World! You funny!"),
-		static=False
-	)
-'''
-@app.external
-def set_app_global_state_value(str: abi.String) -> Expr:
-	return app.state.my_desc.set(str.get())
-@app.external
-def read_app_global_state_value(*, output: abi.String) -> Expr:
-	return output.set(app.state.my_desc)
-'''
-
-# Reserved Global State Value
-class ReservedState:
-	names = ReservedGlobalStateValue(
-		stack_type=TealType.bytes,
-		max_keys=64,
-		descr="Dictionary of names with 64 possible keys"
-	)
-'''
-@app.external
-def set_app_reserved_global_state_value(k: abi.Uint64, str: abi.String) -> Expr:
-	return app.state.names[k].set(str.get())
-@app.external
-def read_app_reserved_global_state_value(k: abi.Uint64, *, output: abi.String) -> Expr:
-	return output.set(app.state.names[k])
-'''
-
-# Local State Value
-class LocalState:
-	count = LocalStateValue(
-		stack_type=TealType.uint64,
-		default=Int(1),
-		descr="A counter that keeps track  of counts"
-	)
-'''
-@app.external
-def increase_local_state(v: abi.Uint64) -> Expr:
-	return app.state.count[Txn.sender()].increment(v.get())
-
-@app.external
-def get_local_state(*, output: abi.Uint64) -> Expr:
-	return output.set(app.state.count[Txn.sender()])
-'''
-
-# Reserved Local State Value
-class ReservedLocalState:
-	food = ReservedLocalStateValue(
-		stack_type=TealType.bytes,
-		max_keys=8,
-		descr="* key value pairs of different foods"
-	)
-'''
-@app.external
-def set_res_ls(k: abi.Uint8, v: abi.String) -> Expr:
-	return app.state.food[k][Txn.sender()].set(v.get())
-
-@app.external(read_only=True)
-def read_res_ls(k: abi.Uint8, *, output: abi.String) -> Expr:
-	return output.set(app.state.food[k][Txn.sender()])
-'''
-
-# Local State Blob
-class LocalStateBlob:
-	blob = LocalStateBlob(
-		keys=2,
-		descr="A blob of 254 bytes"
+def bootstrap() -> Expr:
+	return Seq(
+		Pop(app.state.addr_list.create()),
+		app.initialize_global_state()
 	)
 
-app = Application("LocalState Blob", state=LocalStateBlob()).apply(
-	unconditional_opt_in_approval, initialize_local_state=True
-)
-
-@app.external(authorize=Authorize.opted_in())
-def write_blob(v: abi.String) -> Expr:
-	return app.state.blob.write(Int(0), v.get())
+@app.external
+def vote(addr: abi.Address) -> Expr:
+	return Seq(app.state.addr_list[app.state.votes].set(addr), app.state.votes.increment())
 
 @app.external
-def read_local_blob(*, output: abi.String) -> Expr:
-	return output.set(
-		app.state.blob.read(
-			Int(0), app.state.blob.blob.max_bytes - Int(1)
-		)
+def readVote(vote: abi.Uint64, *, output: abi.Address) -> Expr:
+	return app.state.addr_list[vote.get()].store_into(output)
+
+@app.external
+def readGlobal(*, output: abi.Uint64) -> Expr:
+	return output.set(app.state.votes.get())
+
+
+@app.external
+def addParliamentSeat(area: abi.String, no: abi.Uint8, state: abi.String, candidate_no: abi.Uint8, candidate_name: abi.String, candidate_party: abi.String, votes: abi.Uint64) -> Expr:
+	box_tuple = Parliament()
+	ret = Seq(
+		box_tuple.set(area, no, state, candidate_no, candidate_name, candidate_party, votes),
+		app.state.par_seat[area.get()].set(box_tuple)
 	)
+	return ret
 
-'''
-@app.opt_in
-def opt_in() -> Expr:
-	return Approve()
+@app.external
+def readParliamentItemState(area: abi.String, *, output: abi.String) -> Expr:
+    ret = output.set(app.state.par_seat[area.get()].get())
+    return ret
 
-@app.close_out
-def close_out() -> Expr:
-	return Reject()
+'''@app.external
+def updateParliamentSeatVotes(area: abi.String, new_votes: abi.Uint64) -> Expr:
+    current_item = ParliamentItem()
+    app.state.par_seat[area.get()].store_into(current_item)
 
-@app.clear_state
-def clear_state() -> Expr:
-	return Approve()
-'''
+
+    # Store the updated item back in the Box
+    ret = Seq(
+        app.state.par_seat[area.get()].set(updated_item)
+    )
+    return ret'''
+
+'''@app.external
+def readParliamentSeat(area: abi.String, *, output: ParliamentItem) -> Expr:
+	return app.state.par_seat[area.get()].store_into(output)
+
+@app.external
+def deleteParliamentSeat(area: abi.String) -> Expr:
+	return Pop(app.state.par_seat[area.get()].delete())'''
